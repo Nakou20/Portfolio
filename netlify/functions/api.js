2c2a42740ff0e92ad5d58b8f8177c7a1bb271389
sasha mangin2006 <sashamangin2006@gmail.com>
2026-04-28 12:11:24 +0200

    changment de l api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,7 @@
 const serverless = require('serverless-http');
 const app = require('../../app');
 
+// Forcer Netlify à inclure EJS dans le bundle de la fonction
+require('ejs');
+
 module.exports.handler = serverless(app);
